7451915738410a9b97df86dcf9b6721cc7dbc6c1
Satish Shriwas <rockblog123@gmail.com>
2021-10-01 19:06:18 +0530

    Update

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,26 +1,30 @@
-import firebase from '../Firebase';
+import firebase from "../Firebase";
 var provider = new firebase.auth.GoogleAuthProvider();
 
-export const signInWithGoogle=async()=>{
-    let user;
-  await firebase.auth()
-.signInWithPopup(provider)
-.then((result) => {
-
-console.log(result);
-user=result.user;
-}).catch((error) => {
-   console.log(error);
-});
-return user;
-}
-export const logoutUser=async()=>{
-    let islogout;
-    await firebase.auth().signOut().then(() => {
-        islogout=true;
-       
-       }).catch((error) => {
-          console.log(error);
-       });
-       return islogout;
-}
+export const signInWithGoogle = async () => {
+  let user;
+  await firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      console.log("-->>", result);
+      user = result.user;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return user;
+};
+export const logoutUser = async () => {
+  let islogout;
+  await firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      islogout = true;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return islogout;
+};
